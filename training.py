254f60f18bccaea9ea78c8f3138e20c0670ef922
ashaller2017 <71198197+ashaller2017@users.noreply.github.com>
2021-04-12 20:38:34 -0400

    Add files via upload

---
+++ training.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import json 
+import numpy as np 
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from sklearn.preprocessing import LabelEncoder
+
+
+# In[20]:
+
+
+with open('AIbot.json') as file:
+    data = json.load(file)
+
+
+# In[25]:
+
+
+training_sentences = []
+training_labels = []
+labels = []
+responses = []
+
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        training_sentences.append(pattern)
+        training_labels.append(intent['tag'])
+    responses.append(intent['responses'])
+    
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+        
+num_classes = len(labels)
+print(labels)
+
+
+# In[23]:
+
+
+lbl_encoder = LabelEncoder()
+lbl_encoder.fit(training_labels)
+training_labels = lbl_encoder.transform(training_labels)
+
+
+# In[15]:
+
+
+vocab_size = 1000
+embedding_dim = 16
+max_len = 20
+oov_token = "<OOV>"
+
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token) # adding out of vocabulary token
+tokenizer.fit_on_texts(training_sentences)
+word_index = tokenizer.word_index
+sequences = tokenizer.texts_to_sequences(training_sentences)
+padded_sequences = pad_sequences(sequences, truncating='post', maxlen=max_len)
+
+
+# In[16]:
+
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim, input_length=max_len))
+model.add(GlobalAveragePooling1D())
+model.add(Dense(16, activation='relu'))
+model.add(Dense(16, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+
+# In[17]:
+
+
+epochs = 550
+history = model.fit(padded_sequences, np.array(training_labels), epochs=epochs)
+
+
+# In[18]:
+
+
+
+
+# saving model
+model.save("chat_model")
+
+import pickle
+
+# saving tokenizer
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
+
+# saving label encoder
+with open('label_encoder.pickle', 'wb') as ecn_file:
+    pickle.dump(lbl_encoder, ecn_file, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+# In[ ]:
+
+
+
+
